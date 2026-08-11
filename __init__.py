dd45cf12c5130af724cbaf6df4de031e6c07f82b
Emagore <39279949+Emagore@users.noreply.github.com>
2026-08-11 13:45:01 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,9 @@
+"""Mission Logis Impec."""
+from __future__ import annotations
 
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    return True
